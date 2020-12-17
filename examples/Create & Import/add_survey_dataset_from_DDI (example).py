28f9fcdc0fa70e35eea5d8ff09d4f3993bd44df8
Kamwoo Lee <kl9ch@virginia.edu>
2020-12-17 13:49:39 -0500

    import ddi example added

---
+++ examples/Create & Import/add_survey_dataset_from_DDI (example).py
@@ -0,0 +1,32 @@
+import pynada as nada
+import inspect
+
+nada.set_api_url('http://training.ihsn.org/index.php/api/')
+api_key = 'cf16a23a3cfc6a928f63dd3c8daf8796'
+nada.set_api_key(api_key)
+
+######################################
+# add_survey_dataset_from_DDI example
+######################################
+
+file = "SURVEY_DATASET_SAMPLE_02.xml"
+overwrite = "yes"
+repository_id = "central"
+access_policy = "open"
+published = 1
+
+response = nada.add_survey_dataset_from_DDI(
+    file=file,
+    overwrite=overwrite,
+    repository_id=repository_id,
+    access_policy=access_policy,
+    # data_remote_url=data_remote_url,
+    # rdf=rdf,
+    # published=published
+)
+
+print(response)
+
+dataset_id = response['survey']['idno']
+thumbnail_path = nada.text_to_thumbnail("Survey\nDataset")
+nada.add_thumbnail(dataset_id, thumbnail_path)
