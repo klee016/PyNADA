8dd7f1141bd7aed5849679da950cbe915b4a4c7b
Kamwoo Lee <kl9ch@virginia.edu>
2020-12-17 22:13:00 -0500

    more examples added

---
+++ examples/Manage & Update/manage_and_update_test.py
@@ -0,0 +1,26 @@
+import pynada as nada
+
+nada.set_api_url('http://training.ihsn.org/index.php/api/')
+api_key = 'cf16a23a3cfc6a928f63dd3c8daf8796'
+nada.set_api_key(api_key)
+
+# idno = "WPS8038"
+# nada.delete_dataset(idno)
+
+#print(nada.update_survey('ALB_2000_MICS_V01_M'))
+
+#nada.delete_dataset('string')
+
+#nada.upload_file('study_id', 'WBG-logo.jpg')
+
+#nada.delete_file('study_id', 'WBG-logo.jpg')
+
+#nada.upload_file('SCRIPT_DATASET_SAMPLE_01', 'predicting_food_crises.R')
+
+nada.add_resource(
+	dataset_id="SCRIPT_DATASET_SAMPLE_01",
+	dctype="Document [doc/oth]",
+	title="predicting_food_crises",
+	filename="predicting_food_crises.R",
+	overwrite="yes"
+)
---
+++ examples/Search & Browse/search_and_browse_test.py
@@ -0,0 +1,28 @@
+import pynada as nada
+
+nada.set_api_url('http://training.ihsn.org/index.php/api/')
+api_key = 'cf16a23a3cfc6a928f63dd3c8daf8796'
+nada.set_api_key(api_key)
+
+result = nada.list_datasets()
+# print("okay")
+
+#print(nada.list_collections())
+
+#print(nada.list_datasets())
+
+#print(nada.get_dataset_info('IMAGE-DATASET-SAMPLE-01'))
+
+#print(nada.list_tags('IMAGE-DATASET-SAMPLE-01'))
+
+#print(nada.list_aliases('TIMESERIES-DATASET-SAMPLE-01'))
+
+#print(nada.list_variables('ALB_2005_LSMS_V01_M'))
+
+#print(nada.get_variable_info('ALB_2005_LSMS_V01_M', 'V1'))
+
+#print(nada.list_resources('BGD_RRRC-UNHCR-IOM_20190108'))
+
+#print(nada.list_datafiles('BGD_RRRC-UNHCR-IOM_20190108'))
+
+#print(nada.list_files('survey-doc-01'))
