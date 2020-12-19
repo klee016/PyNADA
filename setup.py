145d09b3ca6cbea28e15c6c9aa1a544a9a8d5748
Kamwoo Lee <kl9ch@virginia.edu>
2020-12-18 19:14:02 -0500

    version up

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 BASE_DIR = pathlib.Path(__file__).parent
 
 PACKAGE_NAME = 'pynada'
-VERSION = '0.0.16'
+VERSION = '0.0.17'
 AUTHOR = 'Kamwoo Lee'
 URL = 'https://github.com/kl9ch/PyNADA'
 
