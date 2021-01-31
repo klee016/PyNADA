f9908827610ed2bd70097e01c216821c092ed606
Kamwoo Lee <kl9ch@virginia.edu>
2021-01-31 18:17:11 -0500

    API key removed

--- examples/Create & Import/create_survey_dataset_from_DDI (example).py
+++ examples/Create & Import/create_survey_dataset_from_DDI (example).py
@@ -2,7 +2,7 @@ import pynada as nada
 import inspect
 
 nada.set_api_url('http://training.ihsn.org/index.php/api/')
-api_key = 'cf16a23a3cfc6a928f63dd3c8daf8796'
+api_key = open("API Key.txt", "r").read()
 nada.set_api_key(api_key)
 
 ######################################
