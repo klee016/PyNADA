bf9d5eb3b25e9511e6a85f3eb5734bc56f5147a5
Kamwoo Lee <kl9ch@virginia.edu>
2021-03-18 11:04:05 -0400

    removed folder from repository

--- tests/pdf_to_thumbnail_test.py
+++
@@ -1,5 +0,0 @@
-from pynada import utils
-
-pdf_file_path = './WPS8038.pdf'
-thumbnail_path = utils.pdf_to_thumbnail(pdf_file_path, page_no = 1)
-print(thumbnail_path)
--- tests/text_to_thumbnail_test.py
+++
@@ -1,3 +0,0 @@
-from pynada import utils
-
-utils.text_to_thumbnail("Visualization\nDataset")
